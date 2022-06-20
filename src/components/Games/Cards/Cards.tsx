53b654b7579affe18f762814aea755868c5f71e3
mishashmidt <mishashmidt0@gmail.com>
2022-06-20 21:03:43 +0300

    feat: refactore cards

--- src/components/Games/Cards/Cards.tsx
+++ src/components/Games/Cards/Cards.tsx
@@ -1,30 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import s from "./styleCards.module.css"
 import {useDispatch, useSelector} from "react-redux";
-import {setCardsTC} from "../../../store/cardsSlice";
+import {cardsType, setCardsTC} from "../../../store/cardsSlice";
 import {Dispatch} from "@reduxjs/toolkit";
 import {storeType} from "../../../store/redux";
 import {Preloader} from "./Preloader";
 import {Card} from "./Card";
 import {allFilters, search, sort} from "./ustilFiltersAndSorting";
+import {filterType} from "../../../store/filterValueSlice";
+import {filterRangeAndSortType} from "../../../store/filterRangeAndSortSlice";
 
 
-export const Cards =() => {
+export const Cards = () => {
     const dispatch = useDispatch<Dispatch<any>>()
-    const {cards, filterValue, filter} = useSelector<storeType, storeType>(state => state)
+    const cards = useSelector<storeType, cardsType[]>(state => state.cards)
+    const filterValue = useSelector<storeType, filterType>(state => state.filterValue)
+    const filter = useSelector<storeType, filterRangeAndSortType>(state => state.filter)
 
     useEffect(() => {
         dispatch(setCardsTC())
     }, [])
 
 
-    const filteredArr = cards.filter(card => {
+    const filteredArr = useMemo(() => cards.filter(card => {
         return allFilters(card, filterValue, filter)
-    })
+    }), [cards, filterValue, filter])
 
-    const sorting = sort(filteredArr, filter.sort)
+    const sorting = useMemo(() => sort(filteredArr, filter.sort), [filteredArr, filter.sort])
 
-    const searching = search(sorting, filter.search)
+    const searching = useMemo(() => search(sorting, filter.search), [filteredArr, filter.search])
 
     if (!cards.length) return <Preloader/>
     return (
